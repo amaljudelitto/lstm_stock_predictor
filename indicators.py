32ceb9e47c051d386ae31a1d610d275c4123bc3a
amaljudelitto <amaljudelitto@gmail.com>
2025-06-19 20:27:09 +0530

    Create indicators.py

---
+++ indicators.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def add_indicators(df):
+    df['EMA20'] = df['Close'].ewm(span=20, adjust=False).mean()
+    df['RSI'] = compute_rsi(df['Close'], 14)
+    
+    # MACD
+    exp1 = df['Close'].ewm(span=12, adjust=False).mean()
+    exp2 = df['Close'].ewm(span=26, adjust=False).mean()
+    df['MACD'] = exp1 - exp2
+    
+    return df
+
+def compute_rsi(series, period=14):
+    delta = series.diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
+    RS = gain / loss
+    return 100 - (100 / (1 + RS))
