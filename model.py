a2e24200df8e5a295943314a47291f5aa987cf62
amaljudelitto <amaljudelitto@gmail.com>
2026-04-24 18:19:02 +0530

    Fix LSTM input shape and adjust training epochs
    Updated LSTM input shape and increased training epochs.

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ from tensorflow.keras.layers import LSTM, Dense
 
 def train_and_predict(X_train, y_train, X_test, y_test):
     model = Sequential()
-    model.add(LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    model.add(LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])))
     model.add(LSTM(50))
     model.add(Dense(1))
 
     model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=0)
+    model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=0)
 
     predictions = model.predict(X_test)
     return model, predictions
